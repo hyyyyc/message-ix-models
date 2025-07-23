1e43e3b189cdd177dd77d6a51d8c823d9bdffc7e
Yichu <huang@iiasa.ac.at>
2025-07-23 11:18:16 +0200

    Delete route 9 and add act boudary for existing ibwt

--- message_ix_models/project/geidco25/scenarios/ibwt.py
+++ message_ix_models/project/geidco25/scenarios/ibwt.py
@@ -152,8 +152,8 @@ scen_sour = "baseline_nexus_7_high_ibwt_t3"
 sour_scen = message_ix.Scenario(mp, model=model_sour, scenario=scen_sour)
 
 # Target scenario
-model_tar = "MESSAGE_GLOBIOM_SSP2_v6.1_ibwt_t3.3"
-scen_tar = "baseline_nexus_7_high_ibwt_t3.3"
+model_tar = "MESSAGE_GLOBIOM_SSP2_v6.1_ibwt_t4"
+scen_tar = "baseline_nexus_7_high_ibwt_t4"
 tar_scen = sour_scen.clone(model=model_tar, scenario=scen_tar,
                            keep_solution=False)
 

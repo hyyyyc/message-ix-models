2a2ee71a58f9e433c7dc0d3436a61142aaf76c99
Yichu <huang@iiasa.ac.at>
2025-10-02 11:40:08 +0200

    Brokenaxes for global surface water comparion

--- message_ix_models/project/geidco25/plot/compare_data_sw_ww.py
+++ message_ix_models/project/geidco25/plot/compare_data_sw_ww.py
@@ -20,32 +20,48 @@ plt.rcParams.update({
 CHN_basin = ["Yangtze", "Ganges Bramaputra", "Tarim", "Huang He",
              "Ziya He", "China Coast"]
 
-supply_basins = ["Yangtze", "Ganges Bramaputra", "Mississipy", "Amazon"]
+supply_basins = ["Yangtze", "Ganges Bramaputra",
+                 "Congo", "Mississippi", "Amazon"]
+
+CHN = False
 
 # -------------------------
 # Read surface water sheet
 # -------------------------
-df_sw = pd.read_excel(os.path.join(path, file), sheet_name="sw")
-# filter basin
-df_sw = df_sw[df_sw.basin.isin(CHN_basin)].copy()
-# filter col
-df_sw = df_sw[["basin",
-               "qtot_7p0_low_2025",
-               "China natural runoff_2018",
-               "Water Resource Bulletin_2020",
-               "China water statistical yearbook_2022"]]
-
 rename_dict_sw = {
     "qtot_7p0_high_2020": "qtot_7p0_high (2020)",
     "qtot_7p0_low_2025": "qtot_7p0_low (2025)",
     "China natural runoff_2018": "China Natural Runoff raster dataset (2018)",
     "Water Resource Bulletin_2020": "Water Resource Bulletin (2020)",
-    "China water statistical yearbook_2022": "China Water Statistical Yearbook (2022)"
+    "China water statistical yearbook_2022": "China Water Statistical Yearbook (2022)",
+    "GRADES-hydroDL_2025": "GRADES-hydroDL (2025)"
 }
-df_sw = df_sw.rename(columns=rename_dict_sw)
+
+df_sw = pd.read_excel(os.path.join(path, file), sheet_name="sw")
+if CHN:
+    # filter basin
+    df_sw = df_sw[df_sw.basin.isin(CHN_basin)].copy()
+    # filter col
+    df_sw = df_sw[["basin",
+                   "qtot_7p0_low_2025",
+                   "China natural runoff_2018",
+                   "Water Resource Bulletin_2020",
+                   "China water statistical yearbook_2022"]]
+
+    df_sw = df_sw.rename(columns=rename_dict_sw)
+
+    width_sw = 0.17
+else:
+    # Filter basin
+    df_sw = df_sw[df_sw['basin'] != 'China']
+    # filter col
+    df_sw = df_sw[["basin",
+                   "qtot_7p0_low_2025",
+                   "GRADES-hydroDL_2025"]]
+
+    df_sw = df_sw.rename(columns=rename_dict_sw)
 
 x_sw = np.arange(len(df_sw["basin"]))
-width_sw = 0.17
 colors_sw = [
     "royalblue" if b in supply_basins else "lightcoral" for b in df_sw["basin"]]
 
@@ -53,8 +69,9 @@ colors_sw = [
 # Read water withdrawal sheet
 # -------------------------
 df_ww = pd.read_excel(os.path.join(path, file), sheet_name="ww")
-# filter basin
-df_ww = df_ww[df_ww.basin.isin(CHN_basin)].copy()
+if CHN:
+    # filter basin
+    df_ww = df_ww[df_ww.basin.isin(CHN_basin)].copy()
 
 rename_dict_ww = {
     "Base_RCP7_noint_noIBWT_2030": "Base_RCP7_noint_noIBWT (2030)",
@@ -63,44 +80,95 @@ rename_dict_ww = {
 df_ww = df_ww.rename(columns=rename_dict_ww)
 
 x_ww = np.arange(len(df_ww["basin"]))
-width_ww = 0.35
+width_ww = 0.27
 colors_ww = [
     "royalblue" if b in supply_basins else "lightcoral" for b in df_ww["basin"]]
 
 # -------------------------
 # Plot
 # -------------------------
-fig, axes = plt.subplots(2, 1, figsize=(12, 14))
+# fig, axes = plt.subplots(2, 1, figsize=(12, 14))
 
 # ===== ax1：Surface Water =====
-ax = axes[0]
-ax.bar(x_sw - 3*(width_sw/2),
-       df_sw["qtot_7p0_low (2025)"], width_sw, color=colors_sw)
-ax.bar(x_sw - width_sw/2,
-       df_sw["China Natural Runoff raster dataset (2018)"], width_sw, color=colors_sw, alpha=0.75)
-ax.bar(x_sw + width_sw/2,
-       df_sw["Water Resource Bulletin (2020)"], width_sw, color=colors_sw, alpha=0.5)
-ax.bar(x_sw + 3*(width_sw/2),
-       df_sw["China Water Statistical Yearbook (2022)"], width_sw, color=colors_sw, alpha=0.25)
+# ax = axes[0]
+if CHN:
+    fig, ax = plt.subplots(figsize=(12, 7))
+    ax.bar(x_sw - 3*(width_sw/2),
+           df_sw["qtot_7p0_low (2025)"], width_sw, color=colors_sw)
+    ax.bar(x_sw - width_sw/2,
+           df_sw["China Natural Runoff raster dataset (2018)"], width_sw, color=colors_sw, alpha=0.75)
+    ax.bar(x_sw + width_sw/2,
+           df_sw["Water Resource Bulletin (2020)"], width_sw, color=colors_sw, alpha=0.5)
+    ax.bar(x_sw + 3*(width_sw/2),
+           df_sw["China Water Statistical Yearbook (2022)"], width_sw, color=colors_sw, alpha=0.25)
 
-ax.set_ylabel("km³/yr")
-# ax.set_xlabel("Basin")
-ax.set_title("Comparison of Surface Water")
-ax.set_xticks(x_sw)
-ax.set_xticklabels(df_sw["basin"], rotation=45, ha="right")
+    ax.set_ylabel("km³/yr")
+    # ax.set_xlabel("Basin")
+    ax.set_title("Comparison of Surface Water")
+    ax.set_xticks(x_sw)
+    ax.set_xticklabels(df_sw["basin"], rotation=45, ha="right")
 
-legend_sw = [
-    Patch(facecolor="gray", label="qtot_7p0_low (2025)"),
-    Patch(facecolor="gray", alpha=0.75,
-          label="China Natural Runoff raster dataset (2018)"),
-    Patch(facecolor="gray", alpha=0.5, label="Water Resource Bulletin (2020)"),
-    Patch(facecolor="gray", alpha=0.25,
-          label="China Water Statistical Yearbook (2022)")
-]
-ax.legend(handles=legend_sw)
+    legend_sw = [
+        Patch(facecolor="gray", label="qtot_7p0_low (2025)"),
+        Patch(facecolor="gray", alpha=0.75,
+              label="China Natural Runoff raster dataset (2018)"),
+        Patch(facecolor="gray", alpha=0.5,
+              label="Water Resource Bulletin (2020)"),
+        Patch(facecolor="gray", alpha=0.25,
+              label="China Water Statistical Yearbook (2022)")
+    ]
+    ax.legend(handles=legend_sw)
+else:
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 7), sharex='all',
+                                   gridspec_kw={'height_ratios': [1, 3]})
+    fig.subplots_adjust(hspace=0.05)
+
+    ax1.bar(x_sw - width_ww/2,
+            df_sw["qtot_7p0_low (2025)"], width_ww, color=colors_ww)
+    ax1.bar(x_sw + width_ww/2, df_sw["GRADES-hydroDL (2025)"],
+            width_ww, color=colors_ww, alpha=0.5)
+
+    ax2.bar(x_sw - width_ww/2,
+            df_sw["qtot_7p0_low (2025)"], width_ww, color=colors_ww)
+    ax2.bar(x_sw + width_ww/2, df_sw["GRADES-hydroDL (2025)"],
+            width_ww, color=colors_ww, alpha=0.5)
+
+    ax2.set_ylim(0, 2000)
+    ax1.set_ylim(4400, 5800)
+
+    ax1.spines.bottom.set_visible(False)
+    ax2.spines.top.set_visible(False)
+
+    ax1.tick_params(axis='x', length=0)  # 隐藏x轴刻度
+
+    # 创建轴断刻度线，d用于调节其偏转角度
+    d = 0.5
+    kwargs = dict(marker=[(-1, -d), (1, d)], markersize=10,
+                  linestyle="none", color='k', mec='k', mew=1, clip_on=False)
+    ax1.plot([0, 1], [0, 0], transform=ax1.transAxes, **kwargs)
+    ax2.plot([0, 1], [1, 1], transform=ax2.transAxes, **kwargs)
+
+    ax2.set_ylabel("km³/yr")
+    ax2.yaxis.set_label_coords(-0.07, 0.7)
+    ax2.set_xlabel("Basin")
+    ax1.set_title("Comparison of Surface Water")
+    ax2.set_xticks(x_sw)
+    ax2.set_xticklabels(df_sw["basin"], rotation=45, ha="right")
+
+    legend_sw = [
+        Patch(facecolor="gray", label="qtot_7p0_low (2025)"),
+        Patch(facecolor="gray", alpha=0.5, label="GRADES-hydroDL (2025)")
+    ]
+    ax1.legend(handles=legend_sw)
+
+fig_name = "com_sw_global3.png"
+plt.tight_layout()
+plt.savefig(os.path.join(path, fig_name), dpi=300)
+plt.show()
 
 # ===== ax2：Water Withdrawal =====
-ax = axes[1]
+fig, ax = plt.subplots(figsize=(12, 7))
+# ax = axes[1]
 ax.bar(x_ww - width_ww/2,
        df_ww["Base_RCP7_noint_noIBWT (2030)"], width_ww, color=colors_ww)
 ax.bar(x_ww + width_ww/2, df_ww["Khan_SSP2_RCP60 (2030)"],
@@ -118,7 +186,7 @@ legend_ww = [
 ]
 ax.legend(handles=legend_ww)
 
-fig_name = "com_sw_ww_CHN.png"
+fig_name = "com_ww_global2.png"
 plt.tight_layout()
 plt.savefig(os.path.join(path, fig_name), dpi=300)
 plt.show()
